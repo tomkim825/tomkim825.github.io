7126206deb615affe4b05ac564111f3adfdb7e31
tomkim825 <tk825@hotmail.com>
2018-07-18 15:44:31 -0700

    adding

---
+++ src/components/Portfolio.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Parallax } from 'react-spring';
+import '../index.css';
+
+
+export class Portfolio extends React.Component {
+  render() {
+    return (
+<div>
+        <Parallax.Layer
+          offset={1.115}
+          speed={0.3}
+          style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'center', flexWrap: 'wrap' }}>
+          <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+              <div class="front work1" > </div>
+              <div class="back">
+                <p>Web App w/Azure 
+                <br/>Face Recognition APIs: 
+                <br/>Find your MCU doppleganger
+                <br/><span><a href="https://github.com/tomkim825/Project1-FaceAPI" target="_blank">Github Repo</a></span> <span><a href="https://tomkim825.github.io/Project1-FaceAPI/" target="_blank" style={{color:'red'}}>Demo</a></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+              <div class="front work2" > </div>
+              <div class="back">
+                <p>React Native App using EXPO: 
+                <br/>Digital Business cards
+                <br/><span><a href="https://github.com/tomkim825/meishi" target="_blank">Github Repo</a></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+              <div class="front work3" > </div>
+              <div class="back">
+                <p>Full-Stack app with full CRUD functionality 
+                <br/><span><a href="https://github.com/tomkim825/burger" target="_blank">Github Repo</a></span> <span><a href="https://eat-da-burger--.herokuapp.com/" target="_blank" style={{color:'red'}}>Demo</a></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+              <div class="front work4 bottom" > </div>
+              <div class="back bottom">
+                <p>App using Giphy API built with AJAX and Bootstrap
+                <br/><span><a href="https://github.com/tomkim825/GifGetter/" target="_blank">Github Repo</a></span> <span><a href="https://tomkim825.github.io/GifGetter/" target="_blank" style={{color:'red'}}>Demo</a></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+              <div class="front work5 bottom" > </div>
+              <div class="back bottom">
+                <p>Full CRUD app using Firebase
+                <br/><span><a href="https://github.com/tomkim825/TrainTime/" target="_blank">Github Repo</a></span> <span><a href="https://tomkim825.github.io/TrainTime/" target="_blank" style={{color:'red'}}>Demo</a></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+              <div class="front work6 bottom" > </div>
+              <div class="back bottom">
+                <p>Memory game built in REACT
+                <br/><span><a href="https://github.com/tomkim825/clicky-game" target="_blank">Github Repo</a></span> <span><a href="https://tomkim825.github.io/clicky-game/" target="_blank" style={{color:'red'}}>Demo</a></span></p>
+              </div>
+            </div>
+          </div>
+        </Parallax.Layer>
+        </div>
+    )
+  }
+}
+
+
