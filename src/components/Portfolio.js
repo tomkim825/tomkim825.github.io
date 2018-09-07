a4a26dedea181f251bc517ed7935893df829a5ff
tomkim825 <tk825@hotmail.com>
2018-09-07 15:57:53 -0700

    updating portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -16,8 +16,8 @@ export class Portfolio extends React.Component {
               <div className="front work1 work" > </div>
               <div className="back">
                 <p>Mobile-First PWA using React : 
-                <br/>Does your child look more like mom or dad? Find out w/facial recognition
-                <br/><span><a href="https://github.com/tomkim825/which-parent" target="_blank" rel="noopener noreferrer">Github Repo</a></span> <span><a href="https://tomkim825.github.io/which-parent/" target="_blank" rel="noopener noreferrer" style={{color:'red'}}>Demo</a></span><span><a href="https://www.microsoft.com/en-us/p/mom-or-dad-which-parent/9ncm6gg7vs7t" target="_blank" rel="noopener noreferrer">Published in Windows App store</a></span> </p>
+                <br/>Does your child look more like mom or dad? 
+                <br/><span><a href="https://github.com/tomkim825/which-parent" target="_blank" rel="noopener noreferrer">Github Repo</a></span> <span><a href="https://tomkim825.github.io/which-parent/" target="_blank" rel="noopener noreferrer" style={{color:'red'}}>Demo</a></span><br/><span><a href="https://www.microsoft.com/en-us/p/mom-or-dad-which-parent/9ncm6gg7vs7t" target="_blank" rel="noopener noreferrer" style={{color:'yellow'}}>Published in Windows App store</a></span> </p>
               </div>
             </div>
           </div>
@@ -25,9 +25,9 @@ export class Portfolio extends React.Component {
             <div className="flipper">
               <div className="front work2 work" > </div>
               <div className="back">
-                <p>Web App w/Azure 
-                <br/>Face Recognition APIs: 
+                <p>Web App w/Azure Face AI: 
                 <br/>Find your MCU doppleganger
+                <br/>-- jQuery, Bootstrap, AJAX, Firebase --
                 <br/><span><a href="https://github.com/tomkim825/Project1-FaceAPI" target="_blank" rel="noopener noreferrer">Github Repo</a></span> <span><a href="https://tomkim825.github.io/Project1-FaceAPI/" target="_blank" rel="noopener noreferrer" style={{color:'red'}}>Demo</a></span></p>
               </div>
             </div>
@@ -36,8 +36,9 @@ export class Portfolio extends React.Component {
             <div className="flipper">
               <div className="front work3 work" > </div>
               <div className="back">
-                <p>React Native App using EXPO: 
+                <p>Native Mobile App: 
                 <br/>Digital Business cards
+                <br/>-- React Native, EXPO, Firebase --
                 <br/><span><a href="https://github.com/tomkim825/meishi" target="_blank" rel="noopener noreferrer">Github Repo</a></span></p>
               </div>
             </div>
@@ -47,7 +48,7 @@ export class Portfolio extends React.Component {
               <div className="front work4 work bottom" > </div>
               <div className="back">
                 <p>Full-Stack app showing full CRUD functionality:
-                <br/> MySQL, Node, Express, Heroku
+                <br/> -- Bootstrap, MySQL, Node, Express, Heroku --
                 <br/><span><a href="https://github.com/tomkim825/burger" target="_blank" rel="noopener noreferrer">Github Repo</a></span> <span><a href="https://eat-da-burger--.herokuapp.com/" target="_blank" rel="noopener noreferrer" style={{color:'red'}}>Demo</a></span></p>
               </div>
             </div>
@@ -57,7 +58,7 @@ export class Portfolio extends React.Component {
               <div className="front work5 work bottom" > </div>
               <div className="back bottom">
                 <p>Web App using Giphy API to search GIFs
-                <br/>built with AJAX and Bootstrap
+                <br/>-- jQuery, AJAX, Bootstrap --
                 <br/><span><a href="https://github.com/tomkim825/GifGetter/" target="_blank" rel="noopener noreferrer">Github Repo</a></span> <span><a href="https://tomkim825.github.io/GifGetter/" target="_blank" rel="noopener noreferrer" style={{color:'red'}}>Demo</a></span></p>
               </div>
             </div>
@@ -66,7 +67,8 @@ export class Portfolio extends React.Component {
             <div className="flipper">
               <div className="front work6 work bottom" > </div>
               <div className="back bottom">
-                <p>Web app using Realtime database - Firebase
+                <p>Web app using Realtime database
+                <br/> -- jQuery, Moment.js, Firebase --
                 <br/><span><a href="https://github.com/tomkim825/TrainTime/" target="_blank" rel="noopener noreferrer">Github Repo</a></span> <span><a href="https://tomkim825.github.io/TrainTime/" target="_blank" rel="noopener noreferrer" style={{color:'red'}}>Demo</a></span></p>
               </div>
             </div>
